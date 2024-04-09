aabdf6971aa50456af8c8a4d7848b2b2b1c91aaa
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2024-04-09 14:27:49 +0900

    chore: change deps

--- deps.ts
+++ deps.ts
@@ -1,4 +1,6 @@
 export { partition } from "jsr:@std/collections@^0.221.0/partition";
-export { extname, join, normalize } from "jsr:@std/url@^0.221.0";
+export { extname } from "jsr:@std/url@^0.221.0/extname";
+export { join } from "jsr:@std/url@^0.221.0/join";
+export { normalize } from "jsr:@std/url@^0.221.0/normalize";
 export { escape } from "jsr:@std/regexp@^0.221.0/escape";
 export { isBuiltin } from "node:module";
