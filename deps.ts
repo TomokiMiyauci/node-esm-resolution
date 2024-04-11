7f178a86d2c654f8cddd19033df3dc8e7d64e3b4
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2024-04-11 13:40:08 +0900

    feat(deps): add deps

--- deps.ts
+++ deps.ts
@@ -2,5 +2,7 @@ export { partition } from "jsr:@std/collections@^0.221.0/partition";
 export { extname } from "jsr:@std/url@^0.221.0/extname";
 export { join } from "jsr:@std/url@^0.221.0/join";
 export { normalize } from "jsr:@std/url@^0.221.0/normalize";
+export { fromFileUrl } from "jsr:@std/path@^0.221.0/from-file-url";
 export { escape } from "jsr:@std/regexp@^0.221.0/escape";
 export { isBuiltin } from "node:module";
+export { format } from "jsr:@miyauci/format@^1.1.0";
