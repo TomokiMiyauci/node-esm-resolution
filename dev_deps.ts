230e091e0b571949c4199268747f28bca2d48a52
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2024-04-11 14:34:57 +0900

    chore: add config

---
+++ dev_deps.ts
@@ -0,0 +1,3 @@
+export { describe, it } from "jsr:@std/testing@^0.221.0/bdd";
+export { assertEquals, assertThrows } from "jsr:@std/assert@^0.221.0";
+export { expect } from "jsr:@std/expect@^0.221.0";
