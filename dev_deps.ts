0073db330842e3b28fd0dbfc2eeeaa177a26db72
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2024-04-12 01:32:28 +0900

    chore: add dev deps

--- dev_deps.ts
+++ dev_deps.ts
@@ -2,3 +2,5 @@ export { describe, it } from "jsr:@std/testing@^0.221.0/bdd";
 export { assertEquals, assertThrows } from "jsr:@std/assert@^0.221.0";
 export { expect } from "jsr:@std/expect@^0.221.0";
 export { format } from "jsr:@miyauci/format@^1.1.0";
+export { fromFileUrl } from "jsr:@std/path@^0.221.0";
+export { join } from "jsr:@std/url@^0.221.0";
