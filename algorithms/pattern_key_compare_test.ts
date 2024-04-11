7d06b38adca9615b8a258611da7c3ba6743e5827
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2024-04-11 12:56:28 +0900

    test(pattern_key_compare): add test case

---
+++ algorithms/pattern_key_compare_test.ts
@@ -0,0 +1,33 @@
+import patternKeyCompare from "./pattern_key_compare.ts";
+import { assertEquals, describe, it } from "../dev_deps.ts";
+
+describe("patternKeyCompare", () => {
+  it("should return order", () => {
+    const table: [left: string, right: string, expected: number][] = [
+      ["/", "/", 1],
+      ["a/", "/", -1],
+      ["/", "a/", 1],
+      ["a/", "a/", 1],
+      ["a/", "b/", 1],
+      ["aa/", "b/", -1],
+      ["a/", "bb/", 1],
+      ["*", "*", 0],
+      ["*a", "*", -1],
+      ["*", "*a", 1],
+      ["*a", "*a", 0],
+      ["*a", "*ab", 1],
+      ["*ab", "*a", -1],
+      ["*a", "a*", 1],
+      ["a*", "*a", -1],
+      ["aa*", "a*a", -1],
+      ["aa*", "*aa", -1],
+      ["a*a", "aa*", 1],
+      ["*aa", "a*a", 1],
+      ["a*a", "a*a", 0],
+    ];
+
+    table.forEach(([left, right, expected]) => {
+      assertEquals(patternKeyCompare(left, right), expected);
+    });
+  });
+});
