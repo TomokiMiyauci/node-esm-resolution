12b48621c3d5eb3ff45000a5694b088e7cf8bf92
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2024-04-12 17:38:17 +0900

    test(package_imports_resolve): add test case

--- algorithms/package_imports_resolve_test.ts
+++ algorithms/package_imports_resolve_test.ts
@@ -33,6 +33,49 @@ describe("packageImportsResolve", () => {
     );
   });
 
+  it("should resolve if the package.json contain conditional imports with default", async () => {
+    await expect(
+      packageImportsResolve(
+        "#dep",
+        import.meta.resolve("../tests/node_modules/imports-map/mod.ts"),
+        [],
+        context,
+      ),
+    ).resolves.toEqual(
+      new URL(import.meta.resolve("../tests/node_modules/imports-map/main.js")),
+    );
+  });
+
+  it("should resolve if the package.json contain conditional imports", async () => {
+    await expect(
+      packageImportsResolve(
+        "#dep",
+        import.meta.resolve("../tests/node_modules/imports-map/mod.ts"),
+        ["node"],
+        context,
+      ),
+    ).resolves.toEqual(
+      new URL(import.meta.resolve("../tests/node_modules/imports-map/node.js")),
+    );
+  });
+
+  it("should resolve if the package.json contain conditional imports and the specifier contain star", async () => {
+    await expect(
+      packageImportsResolve(
+        "#internal/main.js",
+        import.meta.resolve("../tests/node_modules/imports-map/mod.ts"),
+        ["node"],
+        context,
+      ),
+    ).resolves.toEqual(
+      new URL(
+        import.meta.resolve(
+          "../tests/node_modules/imports-map/internal/main.js",
+        ),
+      ),
+    );
+  });
+
   it("should throw error if the package.json does not contain imports field", async () => {
     const specifier = "#dep";
     const url = import.meta.resolve(
